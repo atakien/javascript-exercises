3be6cda9c08d5c73c8a1fcfd726bc7ad7348db1e
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-08-24 17:56:39 +0300

    Exercise 4 function created
    To remove single or multiple elements from an
    array, a function is created. arguments that match
    with the first argument array elements are removed
    and pushed to a new empty array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...e) {
+    let newArray = arr;
+    let array = [];
+    for(let i = 0; i < newArray.length; i++){
+        if(!e.includes(newArray[i])){
+            array.push(newArray[i]);
+        }
+    }
+        return array;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;

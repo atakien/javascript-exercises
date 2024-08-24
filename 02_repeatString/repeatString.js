2669d01596a78b2ae9060b2122710b81795e4960
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-08-24 14:12:37 +0300

    Exercise 2 is modified to passed the tests
    A function is created that gets two arguments:
    string and num. The function is modified with
    if statements and a for loop to pass the test
    one by one.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let result = string;
+if(num === 0){
+   return "";
+} else if(num < 0 || num > 1000){
+   return "ERROR"
+} else{
+   for(let i = 1; i < num; i++){
+        result += string;
+     }
+   return result;
+}
 
 };
 

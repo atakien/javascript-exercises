3c6842d2a98be51139350a9aa4214443da76fd3d
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-08-28 22:07:48 +0300

    Created a function that maps book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+const titlesArray = arr.map(book => book.title);
+return titlesArray;
 };
 
 // Do not edit below this line

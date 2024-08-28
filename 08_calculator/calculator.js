d2056766929d09ccc11eea1b4ee5c3dea45368e3
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-08-28 20:46:42 +0300

    Created and tested functions for a calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((cum, item) => cum + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+return arr.reduce((prod, item) => prod * item, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
